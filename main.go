4490b10798387f06570cb181a2c151fd90361c64
Ajay Kakodia <ajay.kakodia@gmail.com>
2022-04-26 23:32:08 +0530

    commit code in dev

--- main.go
+++ main.go
@@ -6,4 +6,8 @@ func main() {
 	fmt.Println("Ajay Yadav Ja tu")
 	//////// ------------------
 	fmt.Println("Commit to dev")
+	// -----------
+	fmt.Println("commit from main")
+	// --------------
+	fmt.Println("Commit to dev again")
 }
