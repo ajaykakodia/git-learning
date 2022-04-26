32b1ff164797199bf4f64740b1c345a6e5173a79
Ajay Kakodia <ajay.kakodia@gmail.com>
2022-04-26 23:43:08 +0530

    remove two lines now

--- main.go
+++ main.go
@@ -9,9 +9,5 @@ func main() {
 	// -----------
 	fmt.Println("commit from main")
 	// --------------
-	fmt.Println("Commit to dev again")
-	// --------------
-	fmt.Println("Add one more line")
-	// --------------
 	fmt.Println("Add 2nd line")
 }
